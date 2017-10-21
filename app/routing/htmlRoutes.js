f6cd09d51a36ce2f1d4a8695aae33b56d5b9c891
gcdwyer <gcdwyer@gmail.com>
2017-10-20 23:18:43 -0400

    added server with routes, created a person object that has name, photos and their score

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the home page.
+
+
+var routes = function(app) {
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+
+}
+
+module.exports = routes;
